cc077aaae392bc365da716d32e2fe58a60b6dafc
phiaseitz <sophia.seitz@students.olin.edu>
2014-07-01 10:27:20 -0400

    Added the android code that plays a sound (and has a spiffy button)

---
+++ PositionalAudio/app/src/main/java/com/example/positionalaudio/MyActivity.java
@@ -0,0 +1,43 @@
+package com.example.positionalaudio;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void playSound(View v){
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.dovecooing);
+        mediaPlayer.start(); // no need to call prepare(); create() does that for you
+    }
+}
