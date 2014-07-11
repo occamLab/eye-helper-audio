9f4abc971c9a01efd7604e755d8e5bc27dea1d5d
phiaseitz <sophia.seitz@students.olin.edu>
2014-07-11 11:31:57 -0400

    Got the code so that it runs again, but there is an infinite loop, still

--- PositionalAudio/app/src/main/java/com/example/positionalaudio/MyActivity.java
+++ PositionalAudio/app/src/main/java/com/example/positionalaudio/MyActivity.java
@@ -5,17 +5,22 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.SeekBar;
 
 
 public class MyActivity extends Activity {
+    Boolean seekbarPressed = false;
+    Boolean buttonStart = false;
+    int seekBarProgress = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
 
-        SeekBar angleSlider = (SeekBar)findViewById(R.id.seekBar);
+        SeekBar angleSlider = (SeekBar) findViewById(R.id.seekBar);
+
         angleSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -27,73 +32,20 @@ public class MyActivity extends Activity {
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
+                seekbarPressed = true;
 
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
 
-                int angleProg = seekBar.getProgress();
-                int angleVal = angleProg - 90;
-                int angleFile;
-                int distance = 10;
-
-                if (angleVal <= -80) {
-                    angleFile = R.raw.angle_85;
-                } else if (angleVal <= -70) {
-                    angleFile = R.raw.angle_75;
-                } else if (angleVal <= -60) {
-                    angleFile = R.raw.angle_65;
-                } else if (angleVal <= -50) {
-                    angleFile = R.raw.angle_55;
-                } else if (angleVal <= -40) {
-                    angleFile = R.raw.angle_45;
-                } else if (angleVal <= -30) {
-                    angleFile = R.raw.angle_35;
-                } else if (angleVal <= -20) {
-                    angleFile = R.raw.angle_25;
-                } else if (angleVal <= -10) {
-                    angleFile = R.raw.angle_15;
-                } else if (angleVal <= 0) {
-                    angleFile = R.raw.angle_5;
-                } else if (angleVal <= 10) {
-                    angleFile = R.raw.angle5;
-                } else if (angleVal <= 20) {
-                    angleFile = R.raw.angle15;
-                } else if (angleVal <= 30) {
-                    angleFile = R.raw.angle25;
-                } else if (angleVal <= 40) {
-                    angleFile = R.raw.angle35;
-                } else if (angleVal <= 50) {
-                    angleFile = R.raw.angle45;
-                } else if (angleVal <= 60) {
-                    angleFile = R.raw.angle55;
-                } else if (angleVal <= 70) {
-                    angleFile = R.raw.angle65;
-                } else if (angleVal <= 80) {
-                    angleFile = R.raw.angle75;
-                } else {
-                    angleFile = R.raw.angle85;
-                }
-
-                long lastPlayTime = System.nanoTime();
-                long ellapsedTime = 0;
-                float waitTime = distance * 1000000000;
-                while (seekBar.isSelected() == false){
-                    ellapsedTime =  System.nanoTime() - lastPlayTime;
-                    if (ellapsedTime > waitTime){
-                        playSound(angleFile);
-                        lastPlayTime = System.nanoTime();
-                    }
-
-
-                }
+                seekbarPressed = false;
+                seekBarProgress = seekBar.getProgress();
 
             }
         });
 
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -114,12 +66,70 @@ public class MyActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void startLoop(View view){
+        buttonStart = !buttonStart;
+        long lastPlayTime = System.nanoTime();
+        int distance = 100;
+        float waitTime = distance * 1000000000;
+        while(!seekbarPressed && buttonStart){
+            int angleVal = seekBarProgress - 90;
+            int angleFile;
+
+
+            if (angleVal <= -80) {
+                angleFile = R.raw.angle_85;
+            } else if (angleVal <= -70) {
+                angleFile = R.raw.angle_75;
+            } else if (angleVal <= -60) {
+                angleFile = R.raw.angle_65;
+            } else if (angleVal <= -50) {
+                angleFile = R.raw.angle_55;
+            } else if (angleVal <= -40) {
+                angleFile = R.raw.angle_45;
+            } else if (angleVal <= -30) {
+                angleFile = R.raw.angle_35;
+            } else if (angleVal <= -20) {
+                angleFile = R.raw.angle_25;
+            } else if (angleVal <= -10) {
+                angleFile = R.raw.angle_15;
+            } else if (angleVal <= 0) {
+                angleFile = R.raw.angle_5;
+            } else if (angleVal <= 10) {
+                angleFile = R.raw.angle5;
+            } else if (angleVal <= 20) {
+                angleFile = R.raw.angle15;
+            } else if (angleVal <= 30) {
+                angleFile = R.raw.angle25;
+            } else if (angleVal <= 40) {
+                angleFile = R.raw.angle35;
+            } else if (angleVal <= 50) {
+                angleFile = R.raw.angle45;
+            } else if (angleVal <= 60) {
+                angleFile = R.raw.angle55;
+            } else if (angleVal <= 70) {
+                angleFile = R.raw.angle65;
+            } else if (angleVal <= 80) {
+                angleFile = R.raw.angle75;
+            } else {
+                angleFile = R.raw.angle85;
+            }
+
+            long elapsedTime =  System.nanoTime() - lastPlayTime;
+            if (elapsedTime > waitTime){
+                playSound(angleFile);
+                lastPlayTime = System.nanoTime();
+            }
+
+
+        }
+    }
 
     public void playSound(int fileResource){
 
         MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), fileResource);
         mediaPlayer.start(); // no need to call prepare(); create() does that for you
     }
+
 
 
 }
